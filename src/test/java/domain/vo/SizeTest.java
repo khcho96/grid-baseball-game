f3bf19748ee06adce8abc41150ab6dafa474af32
khcho96 <khcho1492@gmail.com>
2025-11-12 18:47:14 +0900

    test: SizeTest 테스트 코드 추가

---
+++ src/test/java/domain/vo/SizeTest.java
@@ -0,0 +1,42 @@
+package domain.vo;
+
+import static constant.ErrorMessage.NUMBER_FORMAT_ERROR;
+import static constant.ErrorMessage.RANGE_ERROR;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SizeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1a", "a"})
+    void 숫자가_아닌_형식의_입력값이면_예외를_발생시킨다(String value) {
+        assertThatThrownBy(() -> Size.newInstance(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NUMBER_FORMAT_ERROR.getErrorMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "2", "9"})
+    void 일정한_범위의_입력값이_아니면_예외를_발생시킨다(String value) {
+        assertThatThrownBy(() -> Size.newInstance(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(RANGE_ERROR.getErrorMessage());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3,3", "5,5", "8,8"})
+    void 정상입력(String value, int expectedSize) {
+        // given
+        Size size = Size.newInstance(value);
+
+        // when
+        int result = size.getSize();
+
+        // then
+        assertThat(result).isEqualTo(expectedSize);
+    }
+}
