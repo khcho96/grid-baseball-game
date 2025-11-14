3d9b7dfd1201a70c858eb33739d81cd9d556d288
khcho96 <khcho1492@gmail.com>
2025-11-14 20:36:31 +0900

    feat: 사용자 상태 패널 구현

--- src/main/java/view/mode/battle/panel/BattleGameUserStatePanel.java
+++ src/main/java/view/mode/battle/panel/BattleGameUserStatePanel.java
@@ -1,6 +1,66 @@
 package view.mode.battle.panel;
 
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import view.util.ComponentSetter;
 
 public class BattleGameUserStatePanel extends JPanel {
+
+    private int pitchesCount;
+    private int outCount;
+    private boolean isGameOver = false;
+    private final JLabel titleLabel = new JLabel("나 👤");
+    private final JLabel stateLabel = new JLabel("⚾ 현재 투구 수: " + pitchesCount + "    ⭕ 아웃: " + outCount);
+
+    public int getPitchesCount() {
+        return pitchesCount;
+    }
+
+    public void setUserStateComponents() {
+        ComponentSetter.setComponent(titleLabel, 550, 30, 250, 15, Font.BOLD, 25, Color.BLACK);
+        ComponentSetter.setComponent(stateLabel, 550, 25, 150, 45, Font.BOLD, 20, Color.BLACK);
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public void setGameOver() {
+        isGameOver = true;
+    }
+
+    public void increasePitchesCount() {
+        pitchesCount++;
+    }
+
+    public void increaseOutCount() {
+        outCount++;
+    }
+
+    public boolean isMaxOutCount() {
+        return outCount == 3;
+    }
+
+    public void setUserStatePanel() {
+        ComponentSetter.setPanel(this, 550, 100, 0, 200, null);
+        addComponents();
+    }
+
+    private void addComponents() {
+        add(titleLabel);
+        add(stateLabel);
+    }
+
+    public void updateState() {
+        stateLabel.setText("⚾ 현재 투구 수: " + pitchesCount + "    ⭕ 아웃: " + outCount);
+    }
+
+    public void resetState() {
+        pitchesCount = 0;
+        outCount = 0;
+        isGameOver = false;
+        updateState();
+    }
 }
