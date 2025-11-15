de7c4799ca610dc49082dc03b914b89016fd42ed
khcho96 <khcho1492@gmail.com>
2025-11-15 18:40:29 +0900

    feat: 변수로 사용자와 컴퓨터 역할 분리

--- src/main/java/service/BattleGameService.java
+++ src/main/java/service/BattleGameService.java
@@ -4,43 +4,46 @@ import domain.GridButtons;
 import domain.OutZone;
 import domain.vo.Size;
 import dto.SizeDto;
-import generator.RandomOutGenerator;
+import generator.RandomGenerator;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BattleGameService {
 
-    private GridButtons gridButtons;
-    private OutZone outZone;
+    private GridButtons userGridButtons;
+    private GridButtons computerGridButtons;
+    private OutZone userOutZone;
+    private OutZone computerOutZone;
     private Size size;
     private final List<List<Integer>> outCoordinates = new ArrayList<>();
 
     public SizeDto setInitGame() {
         size = Size.newInstance("5");
-        gridButtons = GridButtons.from(size);
-        setOutZones();
+        userGridButtons = GridButtons.from(size);
+        computerGridButtons = GridButtons.from(size);
+        setUserOutZones();
         return SizeDto.newInstance(Integer.parseInt("5"));
     }
 
-    private void setOutZones() {
+    private void setUserOutZones() {
         List<List<Integer>> outCoordinates = new ArrayList<>();
         while (outCoordinates.size() < 3) {
-            List<Integer> outCoordinate = RandomOutGenerator.generateOutZoneCoordinate(size);
+            List<Integer> outCoordinate = RandomGenerator.generateOutZoneCoordinate(size);
             if (!outCoordinates.contains(outCoordinate)) {
                 outCoordinates.add(outCoordinate);
             }
         }
-        outZone = OutZone.of(outCoordinates);
+        userOutZone = OutZone.of(outCoordinates);
     }
 
     public String handleUserGridButtonEvent(int x, int y) {
-        return gridButtons.computeEventResult(x, y, outZone);
+        return userGridButtons.computeEventResult(x, y, userOutZone);
     }
 
     public void handleComputerGridButtonEvent(int x, int y) {
         outCoordinates.add(List.of(x, y));
         if (outCoordinates.size() == 3) {
-            outZone = OutZone.of(outCoordinates);
+            computerOutZone = OutZone.of(outCoordinates);
         }
     }
 
@@ -51,13 +54,16 @@ public class BattleGameService {
     }
 
     private void changeSize() {
-        gridButtons = GridButtons.from(size);
-        setOutZones();
+        userGridButtons = GridButtons.from(size);
+        setUserOutZones();
     }
 
     public SizeDto handleRestartGame() {
-        setOutZones();
+        setUserOutZones();
         return SizeDto.newInstance(size.getSize());
+    }
 
+    public String computeRandomClickResult(List<Integer> coordinate) {
+        return computerGridButtons.computeEventResult(coordinate.get(0), coordinate.get(1), computerOutZone);
     }
 }
